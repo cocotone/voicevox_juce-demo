27b9c257ba858e362a4dae512710dba778c307d8
Tatsuya Shiozawa <tatsuya.shiozawa@cocotone.jp>
2024-07-07 21:48:12 +0900

    Add query converter for frame_synthesis.

--- AudioPlugin/Source/VoicevoxEngine/AudioQueryConverter.cpp
+++ AudioPlugin/Source/VoicevoxEngine/AudioQueryConverter.cpp
@@ -4,6 +4,21 @@
 namespace cctn
 {
 
+namespace
+{
+// Function to repeat elements in a vector
+template<typename T>
+std::vector<T> repeat(const std::vector<T>& input, const std::vector<T>& repeats) 
+{
+    std::vector<T> result;
+    for (size_t i = 0; i < input.size(); ++i) 
+    {
+        result.insert(result.end(), repeats[i], input[i]);
+    }
+    return result;
+}
+}
+
 voicevox::VoicevoxDecodeSource AudioQueryConverter::convertToDecodeSource(const juce::String& audioQuery)
 {
     voicevox::VoicevoxDecodeSource result;
@@ -14,12 +29,13 @@ voicevox::VoicevoxDecodeSource AudioQueryConverter::convertToDecodeSource(const 
 
     if (audio_query_json.isObject())
     {
+        std::vector<float> f0s;
         if (audio_query_json["f0"].isArray())
         {
             const auto f0_array = *audio_query_json["f0"].getArray();
             for (const auto f0_element : f0_array)
             {
-                result.f0Vector.push_back((float)f0_element);
+                f0s.push_back((float)f0_element);
             }
         }
 
@@ -30,28 +46,44 @@ voicevox::VoicevoxDecodeSource AudioQueryConverter::convertToDecodeSource(const 
                 "frame_length": 2
             },
         */
+        std::vector<std::int64_t> phonemes;
+        std::vector<std::int64_t> phoneme_lengths;
+
         if (audio_query_json["phonemes"].isArray())
         {
             const auto phonemes_array = *audio_query_json["phonemes"].getArray();
             for (const auto phoneme_frame : phonemes_array)
             {
-                for (int idx = 0; idx < (int)phoneme_frame["frame_length"]; idx++)
+                try
                 {
-                    try
-                    {
-                        const auto str_phoneme = phoneme_frame["phoneme"].toString().toStdString();
-                        const float phoneme_index = cctn::StaticPhonemes::getInstance()->getPhonemeIndex(str_phoneme);
-                        result.phonemeVector.push_back((float)phoneme_index);
-                    }
-                    catch (std::runtime_error e)
-                    {
-                        juce::Logger::outputDebugString(e.what());
-                        return voicevox::VoicevoxDecodeSource();
-                    }
+                    const auto str_phoneme = phoneme_frame["phoneme"].toString().toStdString();
+                    const std::int64_t phoneme_index = cctn::StaticPhonemes::getInstance()->getPhonemeIndex(str_phoneme);
+                    const std::int64_t frame_length = phoneme_frame["frame_length"];
+                    phonemes.push_back(phoneme_index);
+                    phoneme_lengths.push_back(frame_length);
+                }
+                catch (std::runtime_error e)
+                {
+                    juce::Logger::outputDebugString(e.what());
+                    return voicevox::VoicevoxDecodeSource();
                 }
             }
         }
 
+        std::vector<float> volumes;
+        if (audio_query_json["volume"].isArray())
+        {
+            const auto volume_array = *audio_query_json["volume"].getArray();
+            for (const auto volume_element : volume_array)
+            {
+                volumes.push_back((float)volume_element);
+            }
+        }
+
+        result.f0Vector = f0s;
+        result.volumeVector = volumes;
+        result.phonemeSize = phonemes.size();
+        result.phonemeVector = repeat(phonemes, phoneme_lengths);
         result.sampleRate = audio_query_json["outputSamplingRate"];
     }
 
